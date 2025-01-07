bceb2d8d5eeded4faf052b789bd42878d5fdd7d0
Weslley Gualberto <146981879+WesRush@users.noreply.github.com>
2025-01-07 11:29:26 -0300

    Dia7: Continuação de POO

--- src/Cem/Dias/RoadmapJava/javacore/Aintroducaoclasses/Bintroducaometodos/dominio/Calculator.java
+++ src/Cem/Dias/RoadmapJava/javacore/Aintroducaoclasses/Bintroducaometodos/dominio/Calculator.java
@@ -6,9 +6,15 @@ public class Calculator {
     //saída = uma mensagem oou qualquer tipo de operação visível
     public void twoNumSum (){
         System.out.println(10+10);
+
     }
     public void twoNumSub (){
         System.out.println(10 - 5);
 
+    }
+    //o metodo ele funciona como um contrato
+    // se há apenas 2 numeros aqui, na classe calculatorteste02 spo aceitará 2 numeros com a característica da variável
+    public void twoNumMult(int num1, int num2){
+        System.out.println(num1 * num2);
     }
 }
---
+++ src/Cem/Dias/RoadmapJava/javacore/Aintroducaoclasses/Bintroducaometodos/test/CalculatorTest02.java
@@ -0,0 +1,10 @@
+package Cem.Dias.RoadmapJava.javacore.Aintroducaoclasses.Bintroducaometodos.test;
+
+import Cem.Dias.RoadmapJava.javacore.Aintroducaoclasses.Bintroducaometodos.dominio.Calculator;
+
+public class CalculatorTest02 {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.twoNumMult(10,20);
+    }
+}
